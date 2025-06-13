a3e32c858124e7f1e9de5db47099a730d54d5c3d
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2025-06-13 16:57:18 +0200

    feat: update header with new functions

--- incl/execution.h
+++ incl/execution.h
@@ -6,7 +6,7 @@
 /*   By: lkramer <lkramer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 17:49:58 by lkramer           #+#    #+#             */
-/*   Updated: 2025/06/13 14:46:16 by lkramer          ###   ########.fr       */
+/*   Updated: 2025/06/13 16:56:56 by lkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,42 +40,49 @@ typedef struct s_data
 	char	**path_file;
 }	t_data;
 
-// utils
+// expansion
+char	*get_env_value(char *arg, char **env); 
+
+/* 
+builtins folder
+*/
+// utils.c
 int		ft_strcmp(const char *s1, const char *s2);
 int		builtins(char **args, char **env);
 int		is_builtin(char *arg);
 void	print_banner(void);
 
-// exit
+// exit.c
 int		exit_builtin(char **args);
 
-// pwd 
+// pwd.c
 int		pwd_builtin(char **args, char *cwd, size_t cwd_size);
 
-// cd
-int		cd_builtin(char *args);
+// cd.c
+int		cd_builtin(char *args, char **env);
 
-// echo
-int		echo_builtin(char **path);
+// echo.c
+int		echo_builtin(char **path, char **env);
+void	echo_env(char *args, char **env);
 
-// env
+// env.c
 int		env_builtin(char **env);
 
-// export 
+// export.c
 int		export_builtin(char **args, char **env);
 char	**assign_var(char **env, char *args);
 char	**assign_var_and_value(char *equal_sign, char **env, char *args);
 int		update_add_var(char **args, char **env);
 int		export_without_var(char **env);
 
-// export utils
+// export_utils.c
 int		valid_identifier(char *arg);
 char	**copy_env(char **env);
 void	sort_env(char **env);
 void	print_export(char **env);
 void	free_env(char **env);
 
-// unset 
+// unset.c
 int		unset_builtin(char **args, char **env);
 void	remove_var_from_env(char *args, char **env);
 
