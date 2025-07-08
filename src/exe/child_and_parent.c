9c8817e8a0948ca874bce513529259c316fa6a01
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2025-07-08 21:47:16 +0200

    feat: first version of execution
    Currently, single commands work. To make it work with multiple commands, the NULL termination needs to be added.

--- src/exe/child_and_parent.c
+++ src/exe/child_and_parent.c
@@ -6,119 +6,302 @@
 /*   By: lkramer <lkramer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 19:19:33 by dtimofee          #+#    #+#             */
-/*   Updated: 2025/06/03 15:18:04 by lkramer          ###   ########.fr       */
+/*   Updated: 2025/07/08 21:41:35 by lkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
-
+#include "../../incl/execution.h"
 
 /*
-static void	first_child(t_data *data, int i, char *argv[])
-{
-	int	error;
+1 input 
+1 output 
+1 command 
+if file descriptor is filled write / read from it. 
+do we have input / output redirections 		
+	*/ 
 
-	if (open_file(data, argv[1], i) == -1)
-		cleanup_and_exit(data, i, 1);
-	error = check_command(data, argv[i + 2]);
-	if (error != 0)
-		cleanup_and_exit(data, i, error);
-	dup2(data->fd_in, STDIN_FILENO);
-	dup2(data->pipe_fd[2 * i + 1], STDOUT_FILENO);
-	close_fds(data, i);
+int	execute_with_pipex_logic(t_command *cmds, int cmd_count, char **envp)
+{
+    int		*pipe_fds;
+    pid_t	*child_pids;
+    int		exit_status;
+    int		i;
+
+    pipe_fds = NULL;
+	i = 0;
+    child_pids = malloc(cmd_count * sizeof(pid_t));
+    exit_status = 0;
+    if (setup_pipes(cmd_count, &pipe_fds) == -1) 
+		return (free(child_pids), 1);
+    while (i < cmd_count)
+    {
+        child_pids[i] = fork();
+        if (child_pids[i] == -1) 
+			return (free(pipe_fds), free(child_pids), perror("fork"), exit(1), 1);
+        if (child_pids[i] == 0)
+            child_process(cmds, i, pipe_fds, envp);
+        else
+            exit_status = parent_process(child_pids[i], pipe_fds, cmd_count, i);
+        i++;
+    }
+    return (free(pipe_fds), free(child_pids), exit_status);
 }
 
-static void	last_child(t_data *data, int i, char *argv[])
+int setup_pipes(int cmd_count, int **pipe_fds)
 {
-	int	error;
-
-	if (open_file(data, argv[data->command.cmd_count + 2], i) == -1)
-		cleanup_and_exit(data, i, 1);
-	error = check_command(data, argv[i + 2]);
-	if (error != 0)
-		cleanup_and_exit(data, i, error);
-	dup2(data->fd_out, STDOUT_FILENO);
-	dup2(data->pipe_fd[2 * i - 2], STDIN_FILENO);
-	close_fds(data, i);
-}
-
-static void	child_process(t_data *data, int i, char *argv[], char *envp[])
-{
-	int	signal;
-	int	error;
-
-	if (i == 0)
-		first_child(data, i, argv);
-	else if (i == data->command.cmd_count - 1)
-		last_child(data, i, argv);
-	else
-	{
-		error = check_command(data, argv[i + 2]);
-		if (error != 0)
-			cleanup_and_exit(data, i, error);
-		dup2(data->pipe_fd[2 * i - 2], STDIN_FILENO);
-		dup2(data->pipe_fd[2 * i + 1], STDOUT_FILENO);
-		close_fds(data, i);
-	}
-	signal = execve(data->command.cmd_path, data->command.cmd_argv, envp);
-	if (signal == -1)
-	{
-		perror("Execution failed");
-		free_array(data->command.cmd_argv);
-		free(data->command.cmd_path);
-		exit(1);
-	}
-}
-
-static void	parent_process(t_data *data, int i)
-{
-	int	status;
-	int	w;
-	int	exit_code;
-
-	w = waitpid(data->child_pid[i], &status, 0);
-	if (w == -1)
-	{
-		perror("waitpid");
-		free_data(data);
-		close(data->pipe_fd[2 * i - 2]);
-		exit(1);
-	}
-	if (WEXITSTATUS(status) != 0 && i == data->command.cmd_count - 1)
-	{
-		exit_code = WEXITSTATUS(status);
-		close(data->pipe_fd[2 * i - 2]);
-		free_data(data);
-		exit(exit_code);
-	}
-	if (i < data->command.cmd_count - 1)
-		close(data->pipe_fd[2 * i + 1]);
-	if (i > 0 && i < data->command.cmd_count)
-		close(data->pipe_fd[2 * i - 2]);
-}
-
-void	interprocess_communication(t_data *data, char *argv[], char *envp[])
-{
-	int	i;
+    int i;
 
 	i = 0;
-	while (i < data->command.cmd_count)
-	{
-		if (i < data->command.cmd_count - 1)
-			create_pipe(data, i);
-		data->child_pid[i] = fork();
-		if (data->child_pid[i] == -1)
-		{
-			perror("Fork failed");
-			free_data(data);
-			exit(1);
-		}
-		else if (data->child_pid[i] == 0)
-			child_process(data, i, argv, envp);
-		else
-			parent_process(data, i);
-		i++;
-	}
+    if (cmd_count <= 1)
+        return (0);
+    *pipe_fds = malloc((cmd_count - 1) * 2 * sizeof(int));
+    if (!*pipe_fds)
+        return (-1);
+    while (i < cmd_count - 1)
+    {
+        if (pipe(*pipe_fds + i*2) == -1)
+            return (-1);
+        i++;
+    }
+    return (0);
 }
 
-*/
+void setup_child_fds(int i, int *pipe_fds, int cmd_count)
+{
+    if (i > 0)
+	{
+        fprintf(stderr, "Connecting stdin to pipe %d\n", pipe_fds[(i-1)*2]);
+        if (dup2(pipe_fds[(i-1)*2], STDIN_FILENO) == -1)
+            perror("dup2 input");
+    }
+    if (i < cmd_count - 1)
+	{
+        fprintf(stderr, "Connecting stdout to pipe %d\n", pipe_fds[i*2+1]);
+        if (dup2(pipe_fds[i*2+1], STDOUT_FILENO) == -1)
+            perror("dup2 output");
+    }
+}
+
+void handle_child_redirections(t_command *cmd, int i, int *pipe_fds, int cmd_count)
+{
+    int j;
+
+    j = 0;
+    while (j < (cmd_count - 1) * 2)
+    {
+        close(pipe_fds[j]);
+        j++;
+    }
+    if (cmd[i].fd_in != STDIN_FILENO)
+	{
+		fprintf(stderr, "Connecting stdin to pipe %d\n", pipe_fds[(i-1)*2]);
+        dup2(cmd[i].fd_in, STDIN_FILENO);
+        close(cmd[i].fd_in);
+    }
+    if (cmd[i].fd_out != STDOUT_FILENO)
+	{
+		fprintf(stderr, "Connecting stdout to pipe %d\n", pipe_fds[i*2+1]);
+        dup2(cmd[i].fd_out, STDOUT_FILENO);
+        close(cmd[i].fd_out);
+    }
+}
+
+void child_process(t_command *cmds, int i, int *pipe_fds, char **envp)
+{
+    int cmd_count;
+
+    if (cmds[i].fd_in < 0)
+        cmds[i].fd_in = STDIN_FILENO;
+    if (cmds[i].fd_out < 0)
+        cmds[i].fd_out = STDOUT_FILENO;
+    fprintf(stderr, "\n=== CHILD PROCESS DEBUG ===\n");
+    fprintf(stderr, "Command index: %d\n", i);
+    fprintf(stderr, "Command: %s\n", cmds[i].cmd);
+    cmd_count = 0;
+    while (cmds[cmd_count].cmd)
+        cmd_count++;
+    setup_child_fds(i, pipe_fds, cmd_count);
+    handle_child_redirections(cmds, i, pipe_fds, cmd_count);
+    if (check_command(&cmds[i]) != 0)
+        exit(cmds[i].exit_status);
+	print_child_debug(cmds, i);
+	printf("\033[0;31mCommand %d path: %s\033[0m\n", i, cmds[i].cmd_path);
+    execve(cmds[i].cmd_path, cmds[i].cmd_args, envp);
+    perror("execve");
+    exit(127);
+}
+
+int parent_process(pid_t pid, int *pipe_fds, int cmd_count, int i)
+{
+    int status;
+    
+    if (i > 0)
+        close(pipe_fds[2*(i-1)]);
+    if (i < cmd_count-1)
+        close(pipe_fds[2*i+1]);
+    waitpid(pid, &status, 0);
+    if (WIFEXITED(status))
+        return WEXITSTATUS(status);
+    if (WIFSIGNALED(status))
+        return 128 + WTERMSIG(status);
+    return (0);
+}
+
+void print_child_debug(t_command *cmd, int i)
+{		   
+    fprintf(stderr, "Executing: %s\n", cmd[i].cmd_path);
+    fprintf(stderr, "With args: ");
+	int k;
+    k = 0;
+    while (cmd[i].cmd_args[k])
+    {
+        fprintf(stderr, "'%s' ", cmd[i].cmd_args[k]);
+        k++;
+    }
+    fprintf(stderr, "\n");
+}
+
+
+
+
+
+/* 
+
+Later add to child process: 
+ printf("\033[0;31mCommand %d path: %s\033[0m\n", i, cmds[i].cmd_path);
+    if (is_builtin(cmds[i].cmd_args[0]))
+    {
+		fprintf(stderr, "Executing builtin: %s\n", cmds[i].cmd_args[0]);
+        exit(builtins(&cmds[i], envp));
+    }
+
+	
+
+
+
+void child_process(t_command *cmds, int i, int *pipe_fds, char **envp)
+{
+	if (cmds[i].fd_in < 0) cmds[i].fd_in = STDIN_FILENO;
+    if (cmds[i].fd_out < 0) cmds[i].fd_out = STDOUT_FILENO;
+	
+	fprintf(stderr, "\n=== CHILD PROCESS DEBUG ===\n");
+    fprintf(stderr, "Command index: %d\n", i);
+    fprintf(stderr, "Command: %s\n", cmds[i].cmd);
+    fprintf(stderr, "fd_in: %d, fd_out: %d\n", cmds[i].fd_in, cmds[i].fd_out);
+
+    int cmd_count = 0;
+    while (cmds[cmd_count].cmd != NULL)
+        cmd_count++;
+    if (i > 0)
+    {
+		fprintf(stderr, "Connecting stdin to pipe %d\n", pipe_fds[(i-1)*2]);
+        if (dup2(pipe_fds[(i - 1) * 2], STDIN_FILENO) == -1)
+            perror("dup2 input");
+    }
+    if (i < cmd_count - 1)
+    {
+		 fprintf(stderr, "Connecting stdout to pipe %d\n", pipe_fds[i*2+1]);
+        if (dup2(pipe_fds[i * 2 + 1], STDOUT_FILENO) == -1)
+            perror("dup2 output");
+    }
+	int j = 0;
+    while (j < (cmd_count - 1) * 2)
+	{
+		close(pipe_fds[j]);
+		j++;
+	}
+    if (cmds[i].fd_in != STDIN_FILENO)
+    {
+        if (dup2(cmds[i].fd_in, STDIN_FILENO) == -1)
+            perror("dup2 fd_in");
+		close(cmds->fd_in);
+    }
+    if (cmds[i].fd_out != STDOUT_FILENO)
+    {
+        if (dup2(cmds[i].fd_out, STDOUT_FILENO) == -1)
+            perror("dup2 fd_out");
+		 close(cmds->fd_out);
+    }
+
+	int actual_in = dup(STDIN_FILENO);
+    int actual_out = dup(STDOUT_FILENO);
+    fprintf(stderr, "ACTUAL FDs after dup2 - in:%d out:%d\n", actual_in, actual_out);
+    close(actual_in);
+    close(actual_out);
+	
+	if (check_command(&cmds[i]) != 0)
+		exit(cmds[i].exit_status);
+    fprintf(stderr, "Executing: %s\n", cmds[i].cmd_path);
+    fprintf(stderr, "With args: ");
+    for (int k = 0; cmds[i].cmd_args[k]; k++) {
+        fprintf(stderr, "'%s' ", cmds[i].cmd_args[k]);
+    }
+    fprintf(stderr, "\n");
+    for (int k = 0; cmds[i].cmd_args[k]; k++) {
+        fprintf(stderr, "'%s' ", cmds[i].cmd_args[k]);
+    }
+    fprintf(stderr, "\n");
+    execve(cmds[i].cmd_path, cmds[i].cmd_args, envp);
+	perror("execve");
+	exit(127);
+    
+}
+
+static int parent_process(t_command *cmds, pid_t pid, int *pipe_fds, 
+                         int cmd_count, int i)
+{
+    int status;
+    
+    if (i > 0) 
+		close(pipe_fds[2*(i-1)]);
+    if (i < cmd_count-1) 
+		close(pipe_fds[2*i+1]);
+
+    waitpid(pid, &status, 0);
+    
+    if (WIFEXITED(status))
+        cmds[i].exit_status = WEXITSTATUS(status);
+    else if (WIFSIGNALED(status))
+        cmds[i].exit_status = 128 + WTERMSIG(status);
+	return cmds[i].exit_status;
+}
+
+int	execute_with_pipex_logic(t_command *cmds, int cmd_count, char **envp)
+{
+	int exit_status = 0;
+    int *pipe_fds = NULL;
+	
+    pid_t *child_pids = malloc(cmd_count * sizeof(pid_t));
+    if (cmd_count > 1) {
+        pipe_fds = malloc((cmd_count - 1) * 2 * sizeof(int));
+		int i = 0;
+        while (i < cmd_count - 1) {
+            if (pipe(pipe_fds + i*2) == -1) {
+                perror("pipe");
+                exit(1);
+            }
+			i++;
+        }
+    }
+	int i = 0;
+    while (i < cmd_count)
+	{
+        child_pids[i] = fork();
+        if (child_pids[i] == -1) {
+            perror("fork");
+            exit(1);
+        }
+        else if (child_pids[i] == 0) {
+            child_process(cmds, i, pipe_fds, envp);
+        }
+        else {
+            parent_process(cmds, child_pids[i], pipe_fds, cmd_count, i);
+        }
+		i++;
+    }
+    free(pipe_fds);
+    free(child_pids);
+	return exit_status;
+} */
+
+
--- src/main.c
+++ src/main.c
@@ -6,44 +6,11 @@
 /*   By: lkramer <lkramer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 17:50:23 by lkramer           #+#    #+#             */
-/*   Updated: 2025/06/11 16:06:24 by lkramer          ###   ########.fr       */
+/*   Updated: 2025/07/04 17:58:20 by lkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/minishell.h"
-
-// char	**cell_split_input(char *input)
-// {
-// 	char			**tokens;
-// 	unsigned int	position;
-// 	size_t			bufsize;
-// 	char			*token;
-
-// 	bufsize = BUFSIZ;
-// 	tokens = malloc(bufsize * sizeof * tokens);
-// 	if (!tokens) {
-//         perror("malloc failed");
-//         exit(EXIT_FAILURE);
-// 	}
-// 	position = 0;
-// 	token = strtok(input, DEL);
-// 	while(token != NULL)
-// 	{
-// 		tokens[position++] = token;
-// 		if(position >= bufsize)
-// 		{
-// 			bufsize *= 2;
-// 			tokens = realloc(tokens, bufsize * sizeof(char *));
-// 			 if (!tokens) {
-//                 perror("realloc failed");
-//                 exit(EXIT_FAILURE);
-//             }
-// 		}
-// 		token = strtok(NULL, DEL);
-// 	}
-// 	tokens[position] = NULL;
-// 	return tokens;
-// }
 
 // REPL
 // READ - Evaluate - Print/ Execute - Loop
@@ -51,33 +18,37 @@ int	main(int argc, char **argv, char **envp)
 {
 
 	char		*input;
+	char		*raw_input = NULL;
 	t_token		*first_token;
 	int			cmd_count;
 	t_command	*cmds_array;
-	int			*pipe_fds;
+	int			exit_status;
+	int			i;
 
-	argv[argc] = "h";
-	envp[0] = "h";
+	(void)argc;
+	(void)argv;
+	exit_status = 0;
 	print_banner();
 	while (1)
 	{
-		input = readline("testshell>$ ");
-		if (!input) {
+		raw_input = readline("testshell>$ ");
+		if (!raw_input) {
 			printf("EOF");
 			break;
 			}
+		i = 0;
+		// White space trimmer
+		input = ft_strtrim(raw_input, " \t\n");
+		free(raw_input);
+		if (!input || !*input)
+		{
+			free(input);
+			continue;
+		}
 		if (*input)
 			add_history(input);
-
-		// char **args = cell_split_input(input);
-		// builtins(args, env);
-		// non_builtins(args, env);
-		// int i = 0;
-		// while (args[i])
-		// {
-		// 	printf("Token[%d]: %s\n",i,  args[i]);
-		// 	i++;
-		// }
+		printf("Raw input: \"%s\"\n", input);
+		// Tokenizer 
 		first_token = NULL;
 		if (get_tokens(input, &first_token))
 			continue ;
@@ -89,29 +60,98 @@ int	main(int argc, char **argv, char **envp)
 			current = current->next;
 		}
 		cmd_count = count_cmd_num(first_token);
-		// printf("cmd count - %d\n", cmd_count);
-		// fflush(0);
 		cmds_array = malloc((cmd_count) * sizeof(t_command));
 		if (!cmds_array)
 		{
 			error_input("malloc failed", 1);
 			continue ;
 		}
+		// Parser
 		if (!parse_input(cmds_array, first_token, cmd_count, envp))
 		{
 			free (cmds_array);
 			continue ;
 		}
-		if (cmd_count > 1)
+		// Debug printer 
+        for (int i = 0; i < cmd_count; i++) {
+            printf("CMD %d: %s\n", i, cmds_array[i].cmd);
+            printf("  ARGS:");
+            for (int j = 0; cmds_array[i].cmd_args[j]; j++)
+                printf(" [%s]", cmds_array[i].cmd_args[j]);
+            printf("\n");
+        }
+		while (i < cmd_count)
+		{
+			if (set_path(&cmds_array[i], envp) == -1)
+			{
+				error_input("Failed to set PATH", 127);
+				break;
+			}
+			i++;
+		}
+		/* printf("Initial ENVP:\n");
+		for (int j = 0; envp[j]; j++)
+    		printf("%s\n", envp[j]); */
+		// Executing builtins
+		if (cmd_count == 1 && is_builtin(cmds_array[0].cmd_args[0]))
+        {
+            if (must_run_in_parent(cmds_array[0].cmd_args[0]))
+            {
+                exit_status = builtins(cmds_array, envp);
+                free_resources(input, cmds_array, cmd_count);
+                continue;
+            }
+        }
+		// Executing everything else
+		exit_status = execute_with_pipex_logic(cmds_array, cmd_count, envp);
+		free_resources(input, cmds_array, cmd_count);
+	}
+	return (exit_status);
+}
+
+
+
+
+
+
+/* 
+// printf("cmd count - %d\n", cmd_count);
+		// fflush(0);
+		
+
+if (cmd_count > 1)
+		{
+			pipe_fds = malloc((cmd_count - 1) * 2 * sizeof(int));
+			int i = 0;
+			while (i < cmd_count - 1) 
+			{
+				if (pipe(pipe_fds + i*2) == -1) 
+				{
+					perror("pipe");
+					exit(1);
+				}
+				i++;
+			}
+		} */
+		/* if (cmd_count > 1)
 		{
 			if (!fill_pipes_fd(pipe_fds, cmd_count))
 			{
 				free(cmds_array);
 				continue ;
 			}
-		}
+		} */
 		//free(args);
-		free(input);
-	}
-	return 0;
-}
+
+
+		
+// char **args = cell_split_input(input);
+		// builtins(args, env);
+		// non_builtins(args, env);
+		// int i = 0;
+		// while (args[i])
+		// {
+		// 	printf("Token[%d]: %s\n",i,  args[i]);
+		// 	i++;
+		// }
+		
