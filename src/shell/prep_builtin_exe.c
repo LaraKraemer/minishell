b9d717191bb6b7e5819dc0add50e2cfebff99a09
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2025-08-21 14:39:33 +0200

    feat: fix previous merge conflict errors

--- src/shell/prep_builtin_exe.c
+++ src/shell/prep_builtin_exe.c
@@ -6,7 +6,7 @@
 /*   By: lkramer <lkramer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 12:57:44 by lkramer           #+#    #+#             */
-/*   Updated: 2025/08/21 14:01:00 by lkramer          ###   ########.fr       */
+/*   Updated: 2025/08/21 14:24:11 by lkramer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,31 +42,11 @@ int	setup_paths(t_shell *sh, char **global_env)
 	return (1);
 }
 
-/*
-Handles execution of builtin commands with redirection support.
-Executes builtin commands directly in parent process.
-Saves and restores original stdout to ensure shell state remains intact.
-*/
 int	handle_builtins(t_shell *sh, char ***global_env)
 {
-	int	saved_stdout;
-
 	if (sh->cmd_count == 1 && is_builtin(sh->cmds_array[0].cmd_args[0]))
 	{
 		sh->exit_code = builtins(&sh->cmds_array[0], global_env);
-		saved_stdout = -1;
-		if (sh->cmds_array[0].fd_out != STDOUT_FILENO)
-		{
-			saved_stdout = dup(STDOUT_FILENO);
-			dup2(sh->cmds_array[0].fd_out, STDOUT_FILENO);
-			close(sh->cmds_array[0].fd_out);
-		}
-		sh->exit_code = builtins(&sh->cmds_array[0], global_env, sh->exit_code);
-		if (saved_stdout != -1)
-		{
-			dup2(saved_stdout, STDOUT_FILENO);
-			close(saved_stdout);
-		}
 		free_resources(sh->input, sh->cmds_array, sh->cmd_count);
 		return (1);
 	}
